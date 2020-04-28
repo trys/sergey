aa8ac8eac8f81ab6abb25542c98b4406e5e124af
Gabriel Rodrigues <jipacoding@gmail.com>
2020-04-28 08:18:43 -0400

    changeItemsByHTMLFallback lib
    Extends `changeItemsByHTML`.
    
    Can match tags inside tags
    (e.g. `<meta foo="<sergey-slot></sergey-slot>">`)
    
    ```js
    changeItemsByHTMLFallback({
      html: '<p><!-- your html code --></p>',
      selector: 'p',
      changeItem: (node: Node) => {
        node.atttribs.class += "foo";
        return (domutils).getOuterHTML(node);
      }
    })
    ```

--- src/lib/html/change-tag/index.js
+++ src/lib/html/change-tag/index.js
@@ -1,2 +1,5 @@
 const changeItemsByHTML = require('./change-items-by-html');
+const changeItemsByHTMLFallback = require('./change-items-by-html-fallback');
 module.exports = { changeItemsByHTML };
+
+module.exports = { changeItemsByHTML, changeItemsByHTMLFallback };
